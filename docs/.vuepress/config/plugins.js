bfe97ffab93bd788144520bcbacbbd1d253ef106
ajn404 <huiyuening00@gmail.com>
2021-04-18 23:19:15 +0800

    try gittalk

--- docs/.vuepress/config/plugins.js
+++ docs/.vuepress/config/plugins.js
@@ -83,11 +83,11 @@ module.exports = [
     {
       choosen: 'gitalk',
       options: {
-        clientID: 'a6e1355287947096b88b',
-        clientSecret: 'f0e77d070fabfcd5af95bebb82b2d574d7248d71',
-        repo: 'blog-gitalk-comment', // GitHub 仓库
-        owner: 'xugaoyi', // GitHub仓库所有者
-        admin: ['xugaoyi'], // 对仓库有写权限的人
+        clientID: '70a96379f130a667a9bf',
+        clientSecret: '38ad82c8e05e4e4bdb8457a762e111fc149ddd25',
+        repo: 'society-page-gittalk-comment', // GitHub 仓库
+        owner: 'ajn404', // GitHub仓库所有者
+        admin: ['ajn404'], // 对仓库有写权限的人
         // distractionFreeMode: true,
         pagerDirection: 'last', // 'first'正序 | 'last'倒序
         id: '<%- (frontmatter.permalink || frontmatter.to.path).slice(-16) %>', //  页面的唯一标识,长度不能超过50
